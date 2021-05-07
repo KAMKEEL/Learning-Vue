97c98d97e382f638c0a7b9fbbd2bb77af579f8e9
KAMKEEL <KamronVaswani@gmail.com>
2021-05-07 09:05:22 -0400

    More customization
    Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <h1>This is home</h1>
     <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, expedita! Dolor illum aspernatur, sit explicabo doloribus dolorum possimus beatae quos ullam soluta inventore sapiente mollitia, repellendus quis! Corrupti, fugiat reprehenderit!</p>
     <p class="pink lighten-4 red--text text--darken-4">Test this</p>
+    <h1 class="display-2">Display Test</h1>
   </div>
 </template>
 
