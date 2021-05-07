880d4777d2c69c179c7c919395c6b46c23b958de
KAMKEEL <KamronVaswani@gmail.com>
2021-05-06 20:21:17 -0400

    Playing with text customization

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>This is home</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, expedita! Dolor illum aspernatur, sit explicabo doloribus dolorum possimus beatae quos ullam soluta inventore sapiente mollitia, repellendus quis! Corrupti, fugiat reprehenderit!</p>
+    <p class="pink lighten-4 red--text text--darken-4">Test this</p>
   </div>
 </template>
 
